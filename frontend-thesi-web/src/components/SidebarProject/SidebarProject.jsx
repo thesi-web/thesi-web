58d75b72d4618bf1af44902c75bd50f4ab2d9c32
João Vitor Serra de Freitas <youanshan@gmail.com>
2025-04-17 20:45:06 -0300

    deploy(thesi-web): adiciona botão opções projeto

--- frontend-thesi-web/src/components/SidebarProject/SidebarProject.jsx
+++ frontend-thesi-web/src/components/SidebarProject/SidebarProject.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './SidebarProject.module.css';
+import Button from '../Button/Button'
 
 const SidebarItem = ({ emoji, label, onClick }) => {
   return (
     <div className={styles.item} onClick={onClick}>
-      <span className={styles.emoji}>{emoji}</span>
-      <span className={styles.label}>{label}</span>
+      <div className={styles.textContainer} >
+        <span className={styles.emoji}>{emoji}</span>
+        <span className={styles.label}>{label}</span>
+      </div>
+      <div className={styles.buttonWrapper}>
+        <div className={styles.projectIcon}><i className="bi bi-three-dots"></i></div>
+      </div>
     </div>
   );
 };
